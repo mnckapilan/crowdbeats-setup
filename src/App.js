e5bfbae772e7c26d9340ad755bed078e732893d2
kapilan <km2717@imperial.ac.uk>
2019-01-30 23:23:37 +0000

    setup client and styling (WIP)

--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// const APIurl = "http://localhost:8888/"
+const APIurl = "http://crowdbeats-host.herokuapp.com/"
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      party_id :"",
+    }
+  }
+
+  updatePartyCode(event){
+    this.setState({partyCode:event.target.value})
+  }
+
+  fetchPartyId(){
+    fetch(APIurl+'party_id').then(function(response) {
+      // The response is a Response instance.
+      // You parse the data into a useable format using `.json()`
+      return response.json();
+    })
+    .then(
+      (response) => {
+        this.setState({party_id:response.party_id});
+      }
+    )
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <header className="App-header">        
+        <a class="link" href={APIurl+'login'} > Login to Spotify </a>
+        <br></br>
+        <button class="ripple" onClick={() => {this.fetchPartyId()}}>Get Party Code</button>
+        <h1>{this.state.party_id}</h1>
+        <a class="link" href={APIurl+'logout'}> Log Out </a>
+      </header>
+    </div>
     );
   }
 }
